10fc30d4c71865a671ba135d6177228760bd93f2
Kit Plummer <kitplummer@gmail.com>
2018-12-24 12:04:19 -0500

    update: uses HTTP headers, set description

--- .packages/linux.sh
+++ .packages/linux.sh
@@ -4,7 +4,13 @@ set -e
 gem install fpm
 
 fpm -s dir -t deb -n kitlang -v 0.1.0-$CC-latest \
+  --description "Kit is a programming language designed for creating concise, high performance cross-platform applications."
   -d "$CC" \
   ~/.local/bin/kitc=/usr/bin/kitc std/=/usr/lib/kit
 
-curl -T kitlang_0.1.0-$CC-latest_amd64.deb -ukitplummer:$BINTRAY_API_KEY https://api.bintray.com/content/kitplummer/kit/kitlang/0.1.0/kitlang_0.1.0-$CC-latest_amd64.deb;deb_distribution=wheezy,jessie,stretch,trusty,xenial,bionic;deb_component=universe;deb_architecture=amd64;publish=1;override=1
+curl -vvv -T kitlang_0.1.0-$CC-latest.deb -ukitplummer:$BINTRAY_API_KEY \
+  -H "X-Bintray-Publish: 1" -H "X-Bintray-Override: 1" \
+  -H "X-Bintray-Debian-Distribution: trusty, xenial, bionic" \
+  -H "X-Bintray-Debian-Component: universe" \
+  -H "X-Bintray-Debian-Architecture: amd64" \
+  https://api.bintray.com/content/kitplummer/kit/kitlang/0.1.0/kitlang_0.1.0-$CC-latest_amd64.deb
