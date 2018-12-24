c5e83a03e06bdbb79f356f4766bc2fdd48cb89d6
Kit Plummer <kitplummer@gmail.com>
2018-12-24 11:30:03 -0500

    fix: publish and override

--- .packages/linux.sh
+++ .packages/linux.sh
@@ -7,4 +7,4 @@ fpm -s dir -t deb -n kitlang -v 0.1.0-$CC-latest \
   -d "$CC" \
   ~/.local/bin/kitc=/usr/bin/kitc std/=/usr/lib/kit
 
-curl -T kitlang_0.1.0-$CC-latest_amd64.deb -ukitplummer:$BINTRAY_API_KEY https://api.bintray.com/content/kitplummer/kit/kitlang/0.1.0/kitlang_0.1.0-$CC-latest_amd64.deb;deb_distribution=wheezy,jessie,stretch,trusty;deb_component=universe;deb_architecture=amd64?publish=1
+curl -T kitlang_0.1.0-$CC-latest_amd64.deb -ukitplummer:$BINTRAY_API_KEY https://api.bintray.com/content/kitplummer/kit/kitlang/0.1.0/kitlang_0.1.0-$CC-latest_amd64.deb;deb_distribution=wheezy,jessie,stretch,trusty,xenial,bionic;deb_component=universe;deb_architecture=amd64;publish=1;override=1
