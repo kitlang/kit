e210a2be9bf3eb6bcb22bd844d987192a55c156b
Kit Plummer <kitplummer@gmail.com>
2018-12-24 10:36:34 -0500

    fix: no sudo for gem install of fpm, name and path corrections

--- .packages/linux.sh
+++ .packages/linux.sh
@@ -1,9 +1,10 @@
 #!/bin/#!/usr/bin/env bash
+set -e
 
-sudo gem install fpm
+gem install fpm
 
 fpm -s dir -t deb -n kitlang -v 0.1.0-$CC-latest \
   -d "$CC" \
   ~/.local/bin/kitc=/usr/bin/kitc std/=/usr/lib/kit
 
-curl -T 0.1.0-$CC-latest -ukitplummer:$BINTRAY_API_KEY https://api.bintray.com/content/kitplummer/kit/kitlang/0.1.0-$CC-latest_amd64.deb;deb_component=universe;deb_architecture=amd64
+curl -T kitlang_0.1.0-$CC-latest_amd64.deb -ukitplummer:$BINTRAY_API_KEY https://api.bintray.com/content/kitplummer/kit/kitlang/0.1.0/kitlang_0.1.0-$CC-latest_amd64.deb;deb_component=universe;deb_architecture=amd64
