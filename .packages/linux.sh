3675e308d763524e5f7cb3c1789a2731e18157ed
Kit Plummer <kitplummer@gmail.com>
2018-12-24 14:55:19 -0500

    fix: newlines for fpm

--- .packages/linux.sh
+++ .packages/linux.sh
@@ -1,20 +1,22 @@
-#!/bin/#!/usr/bin/env bash
+#!/usr/bin/env bash
 set -e
 
 gem install fpm
 
-fpm -s dir -t deb -n kitlang -v 0.1.0-$CC-latest \
+VERSION=`grep -r version: package.yaml | cut -d "'" -f2`
+
+fpm -s dir -t deb -n kitlang -v $VERSION-$CC-latest \
   --description "Kit is a programming language designed for creating concise, high performance cross-platform applications." \
-  --license "LGPLv3.0,MIT(See Documentation)"
-  --vendor "kitlang.org"
-  --maintainer "kitlang.org"
-  --url "https://kitlang.org"
+  --license "LGPLv3.0" \
+  --vendor "kitlang.org" \
+  --maintainer "Ben Morris <ben@bendmorris.com>" \
+  --url "https://kitlang.org" \
   -d "$CC" \
   ~/.local/bin/kitc=/usr/bin/kitc std/=/usr/lib/kit
 
-curl -vvv -T kitlang_0.1.0-$CC-latest_amd64.deb -ukitplummer:$BINTRAY_API_KEY \
+curl -vvv -T kitlang_$VERSION-$CC-latest_amd64.deb -ukitplummer:$BINTRAY_API_KEY \
   -H "X-Bintray-Publish: 1" -H "X-Bintray-Override: 1" \
   -H "X-Bintray-Debian-Distribution: trusty,xenial,bionic" \
   -H "X-Bintray-Debian-Component: universe" \
   -H "X-Bintray-Debian-Architecture: amd64" \
-  https://api.bintray.com/content/kitplummer/kit/kitlang/0.1.0/kitlang_0.1.0-$CC-latest_amd64.deb
+  https://api.bintray.com/content/kitplummer/kit/kitlang/0.1.0/kitlang_$VERSION-$CC-latest_amd64.deb
