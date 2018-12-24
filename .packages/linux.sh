e69dd6733ce5ba5272c032b0a003c695a4395dfb
Kit Plummer <kitplummer@gmail.com>
2018-12-24 10:12:36 -0500

    update: add package-specific script, deploy latest to bintray

---
+++ .packages/linux.sh
@@ -0,0 +1,9 @@
+#!/bin/#!/usr/bin/env bash
+
+sudo pip install fpm
+
+fpm -s dir -t deb -n kitlang -v 0.1.0-$CC-latest \
+  -d "$CC" \
+  ~/.local/bin/kitc=/usr/bin/kitc std/=/usr/lib/kit
+
+curl -T 0.1.0-$CC-latest -ukitplummer:$BINTRAY_API_KEY https://api.bintray.com/content/kitplummer/kit/kitlang/0.1.0-$CC-latest_amd64.deb;deb_component=universe;deb_architecture=amd64
