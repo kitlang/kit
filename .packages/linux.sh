6ff2a48d3464e632a7dc618f4c243be95b65545c
Kit Plummer <kitplummer@gmail.com>
2018-12-24 14:29:36 -0500

    fix: remove spaces in distro list

--- .packages/linux.sh
+++ .packages/linux.sh
@@ -10,7 +10,7 @@ fpm -s dir -t deb -n kitlang -v 0.1.0-$CC-latest \
 
 curl -vvv -T kitlang_0.1.0-$CC-latest_amd64.deb -ukitplummer:$BINTRAY_API_KEY \
   -H "X-Bintray-Publish: 1" -H "X-Bintray-Override: 1" \
-  -H "X-Bintray-Debian-Distribution: trusty, xenial, bionic" \
+  -H "X-Bintray-Debian-Distribution: trusty,xenial,bionic" \
   -H "X-Bintray-Debian-Component: universe" \
   -H "X-Bintray-Debian-Architecture: amd64" \
   https://api.bintray.com/content/kitplummer/kit/kitlang/0.1.0/kitlang_0.1.0-$CC-latest_amd64.deb
