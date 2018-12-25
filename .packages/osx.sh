6c1fdf2a59864001b3b0a7b996d9c1de4849c006
Kit Plummer <kitplummer@gmail.com>
2018-12-25 11:34:43 -0500

    fix: version and conditional osx

--- .packages/osx.sh
+++ .packages/osx.sh
@@ -5,7 +5,15 @@ gem install fpm
 
 VERSION=`grep -r version: package.yaml | cut -d "'" -f2`
 
-fpm -s dir -t osxpkg -n kitlang -v $VERSION-$CC-latest \
+if [ "$TRAVIS_BRANCH" == "dev" ] then
+  FILE_VERSION="$VERSION-$CC-rc-$TRAVIS_BUILD_NUMBER"
+fi
+
+if [ "$TRAVIS_BRANCH" == "master" ] then
+  FILE_VERSION="$VERSION-$CC"
+fi
+
+fpm -s dir -t osxpkg -n kitlang -v $FILE_VERSION \
   --description "Kit is a programming language designed for creating concise, high performance cross-platform applications." \
   --license "LGPLv3.0" \
   --vendor "kitlang.org" \
@@ -13,6 +21,6 @@ fpm -s dir -t osxpkg -n kitlang -v $VERSION-$CC-latest \
   --url "https://kitlang.org" \
   ~/.local/bin/kitc=/usr/local/bin/kitc std/=/usr/local/lib/kit
 
-curl -vvv -T kitlang-$VERSION-$CC-latest.pkg -ukitplummer:$BINTRAY_API_KEY \
-  -H "X-Bintray-Publish: 1" -H "X-Bintray-Override: 1" \
-  https://api.bintray.com/content/kitplummer/kit-macos/kitlang/0.1.0/kitlang-$VERSION-$CC-latest.pkg
+  curl -vvv -T kitlang-$FILE_VERSION.pkg -ukitplummer:$BINTRAY_API_KEY \
+    -H "X-Bintray-Publish: 1" -H "X-Bintray-Override: 1" \
+    https://api.bintray.com/content/kitplummer/kit-macos/kitlang/$VERSION/kitlang-$FILE_VERSION.pkg
