fa0bbd733918c7414cad07137a4fdc2491e63d1b
Kit Plummer <kitplummer@gmail.com>
2018-12-26 11:29:56 -0500

    fix: back to single job, conditional deploy in script

--- .packages/osx.sh
+++ .packages/osx.sh
@@ -1,28 +1,33 @@
 #!/usr/bin/env bash
 set -e
 
-gem install fpm --no-ri --no-rdoc
+## Only deploy gcc matrix item
+if [ "$CC" == "gcc" ]; then
 
-VERSION=`grep -r version: package.yaml | cut -d "'" -f2`
+  gem install fpm --no-ri --no-rdoc
 
-if [ "$TRAVIS_BRANCH" == "dev" ]
-then
-  FILE_VERSION=$VERSION-prerelease-$TRAVIS_BUILD_NUMBER
+  VERSION=`grep -r version: package.yaml | cut -d "'" -f2`
+
+  if [ "$TRAVIS_BRANCH" == "dev" ]
+  then
+    FILE_VERSION=$VERSION-prerelease-$TRAVIS_BUILD_NUMBER
+  fi
+
+  if [ "$TRAVIS_BRANCH" == "master" ]
+  then
+    FILE_VERSION=$VERSION-$TRAVIS_BUILD_NUMBER
+  fi
+
+  fpm -s dir -t osxpkg -n kitlang -v $FILE_VERSION \
+    --description "Kit is a programming language designed for creating concise, high performance cross-platform applications." \
+    --license "LGPLv3.0" \
+    --vendor "kitlang.org" \
+    --maintainer "Ben Morris <ben@bendmorris.com>" \
+    --url "https://kitlang.org" \
+    ~/.local/bin/kitc=/usr/local/bin/kitc std/=/usr/local/lib/kit
+
+    curl -vvv -T kitlang-$FILE_VERSION.pkg -ukitplummer:$BINTRAY_API_KEY \
+      -H "X-Bintray-Publish: 1" -H "X-Bintray-Override: 1" \
+      https://api.bintray.com/content/kitplummer/kit-macos/kitlang/$VERSION/kitlang-$FILE_VERSION.pkg
+
 fi
-
-if [ "$TRAVIS_BRANCH" == "master" ]
-then
-  FILE_VERSION=$VERSION-$TRAVIS_BUILD_NUMBER
-fi
-
-fpm -s dir -t osxpkg -n kitlang -v $FILE_VERSION \
-  --description "Kit is a programming language designed for creating concise, high performance cross-platform applications." \
-  --license "LGPLv3.0" \
-  --vendor "kitlang.org" \
-  --maintainer "Ben Morris <ben@bendmorris.com>" \
-  --url "https://kitlang.org" \
-  ~/.local/bin/kitc=/usr/local/bin/kitc std/=/usr/local/lib/kit
-
-  curl -vvv -T kitlang-$FILE_VERSION.pkg -ukitplummer:$BINTRAY_API_KEY \
-    -H "X-Bintray-Publish: 1" -H "X-Bintray-Override: 1" \
-    https://api.bintray.com/content/kitplummer/kit-macos/kitlang/$VERSION/kitlang-$FILE_VERSION.pkg
