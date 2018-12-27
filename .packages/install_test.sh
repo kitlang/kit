436f75d967700c12c1be71354ae5e31634b203fc
Kit Plummer <kitplummer@gmail.com>
2018-12-27 18:16:50 -0500

    update: only install dev and master, by build number

--- .packages/install_test.sh
+++ .packages/install_test.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
+## Grab the version from package.yml
+VERSION=`grep -r version: package.yaml | cut -d "'" -f2`
+
+if [ "$TRAVIS_BRANCH" == "dev" ]; then
+  FILE_VERSION=$VERSION~prerelease
+fi
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  FILE_VERSION=$VERSION
+fi
+
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
 echo "deb https://dl.bintray.com/kitplummer/kit bionic universe" | sudo tee -a /etc/apt/sources.list
 sudo apt update
 apt search kitlang
-sudo apt install kitlang
+sudo apt install kitlang=$FILE_VERSION-$TRAVIS_BUILD_NUMBER
 kitc --version
