e84503e93def6856c107be8062c46fe812577280
Kit Plummer <kitplummer@gmail.com>
2018-12-28 15:53:46 -0500

    fix: only deploy on master or dev branches

--- .packages/osx.sh
+++ .packages/osx.sh
@@ -3,31 +3,31 @@ set -e
 
 ## Only deploy gcc matrix item
 if [ "$CC" == "gcc" ]; then
+  if [ "$TRAVIS_BRANCH" == "dev" ] || [ "$TRAVIS_BRANCH" == "master" ]; then
+    gem install fpm
 
-  gem install fpm
+    VERSION=`grep -r version: package.yaml | cut -d "'" -f2`
 
-  VERSION=`grep -r version: package.yaml | cut -d "'" -f2`
+    if [ "$TRAVIS_BRANCH" == "dev" ]
+    then
+      FILE_VERSION=$VERSION-prerelease-$TRAVIS_BUILD_NUMBER
+    fi
 
-  if [ "$TRAVIS_BRANCH" == "dev" ]
-  then
-    FILE_VERSION=$VERSION-prerelease-$TRAVIS_BUILD_NUMBER
-  fi
+    if [ "$TRAVIS_BRANCH" == "master" ]
+    then
+      FILE_VERSION=$VERSION-$TRAVIS_BUILD_NUMBER
+    fi
 
-  if [ "$TRAVIS_BRANCH" == "master" ]
-  then
-    FILE_VERSION=$VERSION-$TRAVIS_BUILD_NUMBER
-  fi
-
-  fpm -s dir -t osxpkg -n kitlang -v $FILE_VERSION \
-    --description "Kit is a programming language designed for creating concise, high performance cross-platform applications." \
-    --license "LGPLv3.0" \
-    --vendor "kitlang.org" \
-    --maintainer "Ben Morris <ben@bendmorris.com>" \
-    --url "https://kitlang.org" \
-    ~/.local/bin/kitc=/usr/local/bin/kitc std/=/usr/local/lib/kit
+    fpm -s dir -t osxpkg -n kitlang -v $FILE_VERSION \
+      --description "Kit is a programming language designed for creating concise, high performance cross-platform applications." \
+      --license "LGPLv3.0" \
+      --vendor "kitlang.org" \
+      --maintainer "Ben Morris <ben@bendmorris.com>" \
+      --url "https://kitlang.org" \
+      ~/.local/bin/kitc=/usr/local/bin/kitc std/=/usr/local/lib/kit
 
     curl -vvv -T kitlang-$FILE_VERSION.pkg -ukitplummer:$BINTRAY_API_KEY \
       -H "X-Bintray-Publish: 1" -H "X-Bintray-Override: 1" \
       https://api.bintray.com/content/kitplummer/kit-macos/kitlang/$VERSION/kitlang-$FILE_VERSION.pkg
-
+  fi
 fi
