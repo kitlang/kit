3c6cc46fb5f516251c0dc1722b66597fc0a16db2
Chance Snow <git@chancesnow.me>
2021-04-17 00:11:35 -0500

    Expand breadth of AST Operators enumeration

--- src/bootstrap/ast/operators.h
+++ src/bootstrap/ast/operators.h
@@ -3,24 +3,84 @@
 #include <stdlib.h>
 
 enum Operator {
-  Plus,
-  Minus,
-  Division,
-  Modulo,
+  Assign,
+  Inc,
+  Dec,
+  Add,
+  Sub,
+  Div,
+  Mul,
+  Mod,
+  Eq,
+  Neq,
+  Gte,
+  Lte,
+  LeftShift,
+  RightShift,
+  Gt,
+  Lt,
+  And,
+  Or,
+  BitAnd,
+  BitOr,
+  BitXor,
+  Invert,
+  InvertBits,
+  Cons,
 };
 typedef enum Operator Operator;
 
 char* showOperator(Operator op) {
   switch (op)
   {
-  case Plus:
+  case Assign:
+    return "op =";
+  case Inc:
+    return "op ++";
+  case Dec:
+    return "op --";
+  case Add:
     return "op +";
-  case Minus:
+  case Sub:
     return "op -";
-  case Division:
+  case Div:
     return "op /";
-  case Modulo:
+  case Mul:
+    return "op *";
+  case Mod:
     return "op %";
+  case Eq:
+    return 'op =';
+  case Neq:
+    return 'op !=';
+  case Gte:
+    return 'op >=';
+  case Lte:
+    return 'op <=';
+  case LeftShift:
+    return 'op <<';
+  case RightShift:
+    return 'op >>';
+  case Gt:
+    return 'op >';
+  case Lt:
+    return 'op <';
+  case And:
+    return 'op &&';
+  case Or:
+    return 'op ||';
+  case BitAnd:
+    return 'op &';
+  case BitOr:
+    return 'op |';
+  case BitXor:
+    return 'op ^';
+  case Invert:
+    return 'op !';
+  case InvertBits:
+    return 'op ~';
+  case Cons:
+    return 'op ::';
   default:
     return NULL;
   }
